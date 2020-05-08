e6951c2ab84a6490b5cd80fad6c94ea43922de92
xingbofeng <503908971@qq.com>
2020-05-08 16:24:04 +0800

    update comma

--- comma/main.go
+++ comma/main.go
@@ -3,20 +3,43 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"strings"
 )
 
 func main() {
-	fmt.Println(comma("1112345"))
+	fmt.Println(comma("3243511.12341235"))
 }
 
 func comma(s string) string {
 	var buf bytes.Buffer
-	length := len(s)
-	for i, subString := range s {
-		if (length-i)%3 == 0 {
-			buf.WriteString(",")
-		}
-		buf.WriteString(string(subString))
+	lastIndex := strings.LastIndex(s, ".")
+	if lastIndex >= 0 {
+		intPart := s[:lastIndex]
+		insertIntPart(&buf, intPart)
+		buf.WriteString(".")
+		floatPart := s[lastIndex+1:]
+		insertFloatPart(&buf, floatPart)
+		return buf.String()
 	}
+	insertIntPart(&buf, s)
 	return buf.String()
+}
+
+func insertIntPart(bufP *bytes.Buffer, intPart string) {
+	length := len(intPart)
+	for i, subString := range intPart {
+		if (length-i)%3 == 0 && i != 0 {
+			(*bufP).WriteString(",")
+		}
+		(*bufP).WriteString(string(subString))
+	}
+}
+
+func insertFloatPart(bufP *bytes.Buffer, floatPart string) {
+	for i, subString := range floatPart {
+		if i%3 == 0 && i/3 != 0 {
+			(*bufP).WriteString(",")
+		}
+		(*bufP).WriteString(string(subString))
+	}
 }
